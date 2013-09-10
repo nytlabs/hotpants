5fcc798bbdd54e762bb5e7d011ef064e4a442598
Alexis Lloyd <alexislloyd@gmail.com>
2013-09-10 11:24:36 -0400

    added some dreams and dream output

--- sentence_generator.py
+++ sentence_generator.py
@@ -21,6 +21,12 @@ data = {
 				["light", 0, lambda m: m > 0.5 and m < 1.0], ["bright", 0, lambda m: m > 0.6 and m < 1.0], ["beaming", 0, lambda m: m > 0.8 and m < 1.0], ["dark", 0, lambda m: m > 0.0 and m < 0.5], ["murky", 0, lambda m: m > 0.0 and m < 0.5], ["shadowy", 0, lambda m: m > 0.0 and m < 0.5], ["basking", 1, lambda m: m > 0.5 and m < 1.0], ["shaded", 1, lambda m: m > 0.0 and m < 0.5], ["ripening", 1, lambda m: m > 0.5 and m < 1.0], ["ripe", 2, lambda m: m > 0.0 and m < 1.0], ["night", 3, lambda m: m > 0.0 and m < 0.5], ["daytime", 3, lambda m: m > 0.5 and m < 1.0], ["bathed in light", 4, lambda m: m > 0.5 and m < 1.0], ["illuminated", 4, lambda m: m > 0.6 and m < 1.0], ["red", 4, lambda m: m > 0.0 and m < 1.0] , ["light shining on", 5, lambda m: m > 0.5 and m < 1.0], ["shadow falling on", 5, lambda m: m > 0.5 and m < 1.0] 
 
 				]
+			,
+			"dreams" :
+
+				[
+				"I used to be quite small, but I am larger now", "When I was green, there was much more firmness inside", "The time I fell to the ground, there had been less light that day", "I haven't noticed any bees in a while", "There were big green leaves atop my crown"
+				]
 
 			  
 	},
@@ -50,6 +56,12 @@ data = {
 				["tea", lambda m: m > 0.0 and m < 0.5], ["coffee", lambda m: m > 0.0 and m < 0.5], ["cocoa", lambda m: m > 0.0 and m < 0.5], ["hot toddy", lambda m: m > 0.0 and m < 0.5], ["warm milk", lambda m: m > 0.0 and m < 0.5], ["hot cider", lambda m: m > 0.0 and m < 0.5], ["lemonade", lambda m: m > 0.5 and m < 1.0], ["water", lambda m: m > 0.5 and m < 1.0], ["iced tea", lambda m: m > 0.5 and m < 1.0], ["iced coffee", lambda m: m > 0.5 and m < 1.0], ["soda", lambda m: m > 0.5 and m < 1.0], ["seltzer", lambda m: m > 0.5 and m < 1.0], ["cold drink", lambda m: m > 0.5 and m < 1.0], ["hot or cold beverage", lambda m: m > 0.4 and m < 0.6]
 
 				]
+			,
+			"dreams" :
+
+				[
+				"Sometimes I spin around and around and get very hot", "I once sat unused in darkness for a very long time", "There was a day when I was used 15 times. There were many cups of tea.", "When the spoon stirs, it clinks and clanks against my sides", "Ice dropped in makes liquid slosh around me"
+				]
 			  
 	},
 	"BLOCKS" : {
@@ -70,6 +82,12 @@ data = {
 				["busy", 0, lambda m: m > 0.5 and m < 1.0], ["crowded", 0, lambda m: m > 0.8 and m < 1.0], ["packed", 0, lambda m: m > 0.8 and m < 1.0], ["teeming", 0, lambda m: m > 0.8 and m < 1.0], ["populated", 0, lambda m: m > 0.5 and m < 1.0], ["not empty", 0, lambda m: m > 0.2 and m < 1.0], ["empty", 0, lambda m: m > 0.0 and m < 0.3], ["idle", 0, lambda m: m > 0.0 and m < 0.2], ["clear of people", 0, lambda m: m > 0.0 and m < 0.2], ["devoid of people", 0, lambda m: m > 0.0 and m < 0.2], ["someone is", 1, lambda m: m > 0.3 and m < 1.0], ["people are", 1, lambda m: m > 0.3 and m < 1.0], ["a person is", 1, lambda m: m > 0.3 and m < 1.0], ["nobody is", 1, lambda m: m > 0.0 and m < 0.3], ["some people are", 0, lambda m: m > 0.3 and m < 1.0], ["few people are", 1, lambda m: m > 0.0 and m < 0.3], ["no one", 2, lambda m: m > 0.0 and m < 0.2], ["someone", 2, lambda m: m > 0.2 and m < 1.0], ["people", 2, lambda m: m > 0.2 and m < 1.0], ["you", 2, lambda m: m > 0.2 and m < 1.0], ["a group of people", 2, lambda m: m > 0.4 and m < 1.0], ["an assembly of people", 2, lambda m: m > 0.4 and m < 1.0], ["someone", 3, lambda m: m > 0.3 and m < 1.0], ["no one", 3, lambda m: m > 0.0 and m < 0.3], ["you are here", 4, lambda m: m > 0.3 and m < 1.0], ["we have company", 4, lambda m: m > 0.3 and m < 1.0], ["you have gone", 4, lambda m: m > 0.0 and m < 0.3], ["nobody is here", 4, lambda m: m > 0.0 and m < 0.3], ["", 5, lambda m: m > 0.0 and m < 1.0]
 
 				]
+			,
+			"dreams" :
+
+				[
+				"We have seen many more people today than usual", "A long time ago, we sat next to one another in a container", "62 days ago, we were stacked unusually high", "Once, we moved around for a long time before we rested", "There used to be 26 of us", "Sometimes, nobody comes for a long time"
+				]
 			
 	},
 	"modifiers" :
@@ -85,49 +103,54 @@ data = {
 ###  FUNCTION ###
 def generate(obj, reading, diff, dream=False):
 	print 'received %s %s %s %s' % (obj, reading, diff, dream)
-	
-	sentence = data[obj]["sentences"][random.randint(0,len(data[obj]["sentences"])-1)]
-	type = sentence["type"]
-	descriptor = None
-	modifier = ""
+
+	if dream==True:
+		output = data[obj]["dreams"][random.randint(0,len(data[obj]["dreams"])-1)]
+		return output
+
+	else:
+		sentence = data[obj]["sentences"][random.randint(0,len(data[obj]["sentences"])-1)]
+		type = sentence["type"]
+		descriptor = None
+		modifier = ""
 
 
-	while descriptor is None:
-		d = data[obj]["descriptors"][random.randint(0,len(data[obj]["descriptors"])-1)]
-		if d[1] == type:
-			if d[2](reading):
-				descriptor = d[0]
+		while descriptor is None:
+			d = data[obj]["descriptors"][random.randint(0,len(data[obj]["descriptors"])-1)]
+			if d[1] == type:
+				if d[2](reading):
+					descriptor = d[0]
 
-	if obj == "APPLE" or obj == "MUG":
-		if random.random() > 0.5:
-			while modifier is "":
-				m = data["modifiers"][random.randint(0,len(data["modifiers"])-1)]
-				if m[1](reading):
-					modifier = m[0]
+		if obj == "APPLE" or obj == "MUG":
+			if random.random() > 0.5:
+				while modifier is "":
+					m = data["modifiers"][random.randint(0,len(data["modifiers"])-1)]
+					if m[1](reading):
+						modifier = m[0]
 
-	if obj == "MUG":
-		drink = None
-		while drink is None:
-			d = data[obj]["drinks"][random.randint(0,len(data[obj]["drinks"])-1)]
-			if d[1](reading):
-				drink = d[0]
-		output = sentence["string"].replace("DESC", descriptor).replace("MOD", modifier).replace("DRINK", drink).lstrip()
+		if obj == "MUG":
+			drink = None
+			while drink is None:
+				d = data[obj]["drinks"][random.randint(0,len(data[obj]["drinks"])-1)]
+				if d[1](reading):
+					drink = d[0]
+			output = sentence["string"].replace("DESC", descriptor).replace("MOD", modifier).replace("DRINK", drink).lstrip()
 
-	elif obj == "APPLE":
-		output = sentence["string"].replace("DESC", descriptor).replace("MOD", modifier).lstrip()
+		elif obj == "APPLE":
+			output = sentence["string"].replace("DESC", descriptor).replace("MOD", modifier).lstrip()
 
-	elif obj == "BLOCKS":
-		if "DESC" in sentence["string"]:
-			output = sentence["string"].replace("DESC", descriptor).lstrip()
-		else:
-			output = sentence["string"].lstrip()
+		elif obj == "BLOCKS":
+			if "DESC" in sentence["string"]:
+				output = sentence["string"].replace("DESC", descriptor).lstrip()
+			else:
+				output = sentence["string"].lstrip()
 
-	return output[0].upper() + output[1:]
+		return output[0].upper() + output[1:]
 
 
 ### EXAMPLE ###
 # sentence = generate("BLOCKS", random.random(), random.random())
 
-sentence = generate("APPLE", random.random(), random.random())
-print sentence
+# sentence = generate("APPLE", random.random(), random.random(), True)
+# print sentence
 
