6e5ead44bbba9085f89432a9d16bc68cf1b06360
Noah Feehan <nfeehan@gmail.com>
2013-09-13 14:36:44 -0400

    fiddling with remark emission

--- hotpants-TMP102.py
+++ hotpants-TMP102.py
@@ -10,19 +10,16 @@ import random
 import atexit
 import sentence_generator as sg
 
+# emit something every minute
+# map degC to human scale of hot, cold, and temperate
+
 theObj = 'MUG'
+
+humanCold = 20
+humanHot = 30
 
 t = tmp.TMP102()
 readings = []
-# sensor_pin = 'P9_40'
-# extreme_lo = ['dark','inky','shadowed','midnight''black','sinister','dour','glowering','glum','moody','morose','saturnine','sour','sullen','benighted','obscure','blue','dingy','disconsolate','dismal','gloomy','grim','sorry','drab','drear','dreary','colored','coloured','dark-skinned','non-white','depressing','dispiriting']
-# extreme_lo = ['stale','cold','dusty','moth-eaten','frigid','arctic','gelid','glacial','icy','polar','frosty','frozen','wintry','cold-blooded','inhuman','insensate','insentient']
-# mid_lo = ['shady','dim','grey','faint','weak','dim','shadowy','vague','wispy','feeble','light','swooning','light-headed','lightheaded','fainthearted','timid','faint-hearted','cloudy','muddy','murky','turbid']
-# mid_hi = ['light','shiny','clear','lustrous','diaphanous','filmy','gauze-like','gossamer','see-through','sheer','transparent','vaporous','vapourous','cobwebby']
-# extreme_hi = ['blinding','superbright','brilliant','vivid','brilliant','vivid','smart','burnished','lustrous','shining','shiny','undimmed','promising','sunny','sunshiny']
-# extreme_hi = ['raging','hot','angry','furious','tempestuous','wild','blistering','acerbic','acid','acrid','bitter','caustic','sulfurous','sulphurous','virulent','vitriolic','blistery','red-hot','scalding','scathing','venomous','vituperative','juicy','luscious','toothsome','voluptuous','sizzling','live','unrecorded','bouncy','lively','resilient','springy','alive']
-# preamble = ['Now it is hella ','Oh, just a bit ','It is quite ','Gosh it is ','Well looky here, it is ','Suddenly: ','Call the police, it is ','After awhile: ','Things have changed; now it\'s more ','Hey now! It is very ']
-# dream = ['i am falling', 'i am walking and falling', 'i had to take a test', 'i have eaten an embarrassing amount of gum']
 
 def parseLen(text):
     L = []
@@ -88,14 +85,18 @@ def checkSensor():
     rPast = r
 
 def emit_dream(r, delta, avg):
-    norm = mapVals(r,rMin, rMax, 0.0, 0.999)
-    sen = sg.generate(theObj, norm, delta, True)
-    slowPrint(parseLen(sen))
-    printer.feed(2)
-    slowPrint(parseLen('DREAM: '+str(time.ctime()))+'\n')
+    r = random.randint(0,5)
+    if r==1:
+        emit_remark(r, delta, avg)
+    else:
+        norm = mapVals(r,humanCold, humanHot, 0.0, 0.999)
+        sen = sg.generate(theObj, norm, delta, True)
+        slowPrint(parseLen(sen))
+        printer.feed(2)
+        slowPrint(parseLen(str(time.ctime()))+'\n')
 
 def emit_remark(r, delta, avg):
-    norm = mapVals(r,rMin, rMax, 0.0, 0.999)
+    norm = mapVals(r,humanCold, humanHot, 0.0, 0.999)
     sen = sg.generate(theObj, norm, delta, False)
     slowPrint(parseLen(sen))
     printer.feed(2)
@@ -115,7 +116,7 @@ printer = Adafruit_Thermal("/dev/ttyO2", 19200, timeout=5)
 printer.begin()
 printer.upsideDownOn()
 printer.feed(3)
-printer.print('i am awake and I have a TMP102')
+printer.print('i am awake and I am MUG (thermal)')
 printer.feed(1)
 rPast = 0
 rMax = 0 # all-time max sensor reading
--- hotpants-VCNL4000.py
+++ hotpants-VCNL4000.py
@@ -16,15 +16,6 @@ theObj = 'BLOCKS'
 v = vcnl.VCNL4000()
 v.continuousConversionOn()
 readings = []
-# sensor_pin = 'P9_40'
-# extreme_lo = ['dark','inky','shadowed','midnight''black','sinister','dour','glowering','glum','moody','morose','saturnine','sour','sullen','benighted','obscure','blue','dingy','disconsolate','dismal','gloomy','grim','sorry','drab','drear','dreary','colored','coloured','dark-skinned','non-white','depressing','dispiriting']
-# extreme_lo = ['stale','cold','dusty','moth-eaten','frigid','arctic','gelid','glacial','icy','polar','frosty','frozen','wintry','cold-blooded','inhuman','insensate','insentient']
-# mid_lo = ['shady','dim','grey','faint','weak','dim','shadowy','vague','wispy','feeble','light','swooning','light-headed','lightheaded','fainthearted','timid','faint-hearted','cloudy','muddy','murky','turbid']
-# mid_hi = ['light','shiny','clear','lustrous','diaphanous','filmy','gauze-like','gossamer','see-through','sheer','transparent','vaporous','vapourous','cobwebby']
-# extreme_hi = ['blinding','superbright','brilliant','vivid','brilliant','vivid','smart','burnished','lustrous','shining','shiny','undimmed','promising','sunny','sunshiny']
-# extreme_hi = ['raging','hot','angry','furious','tempestuous','wild','blistering','acerbic','acid','acrid','bitter','caustic','sulfurous','sulphurous','virulent','vitriolic','blistery','red-hot','scalding','scathing','venomous','vituperative','juicy','luscious','toothsome','voluptuous','sizzling','live','unrecorded','bouncy','lively','resilient','springy','alive']
-# preamble = ['Now it is hella ','Oh, just a bit ','It is quite ','Gosh it is ','Well looky here, it is ','Suddenly: ','Call the police, it is ','After awhile: ','Things have changed; now it\'s more ','Hey now! It is very ']
-# dream = ['i am falling', 'i am walking and falling', 'i had to take a test', 'i have eaten an embarrassing amount of gum']
 
 def parseLen(text):
     L = []
@@ -122,7 +113,7 @@ printer = Adafruit_Thermal("/dev/ttyO2", 19200, timeout=5)
 printer.begin()
 printer.upsideDownOn()
 printer.feed(3)
-printer.print('i am awake and I have a VCNL4000')
+printer.print('i am awake and I am BLOCKS (presence)')
 printer.feed(1)
 rPast = 0
 rMax = 0 # all-time max sensor reading
