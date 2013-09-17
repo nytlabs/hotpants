ce1dd073227519f0b0b93fcc5b13e7164f642b78
Noah Feehan <nfeehan@gmail.com>
2013-09-17 14:05:21 -0400

    deploying the new parse function which should not break words to accommodate line breaks

--- hotpants-TMP102.py
+++ hotpants-TMP102.py
@@ -46,7 +46,7 @@ def parse(text):
     fin = []
     tally = 0
     for w in r:
-        if len(w)+len(curLine) > (maxCol-1):
+        if len(w)+len(curLine) > (Adafruit_Thermal.maxColumn-1):
             fin.append(curLine)
             curLine = ''
             curLine+=w
