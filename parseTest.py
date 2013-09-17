a90db1abc73cfc7ba77d040cdc98c27b29e5b7fe
Noah Feehan <nfeehan@gmail.com>
2013-09-17 12:00:37 -0400

    adding text method to prevent linebreaks from cutting words in half

---
+++ parseTest.py
@@ -0,0 +1,21 @@
+import Adafruit_Thermal
+
+maxCol = 32
+printer = Adafruit_Thermal("/dev/ttyO2", 19200, timeout=5)
+printer.begin()
+printer.upsideDownOn()
+
+def parseTest(text):
+    r = text.split(' ')
+    # r.reverse()
+    curLine = []
+    fin = []
+    for w in r:
+    	if len(w)+len(curLine) > maxCol:
+    		fin.append(curLine)
+    		curLine = []
+    		curLine.append(w)
+    	else:
+    		curLine.append(w)
+    print fin
+    return
