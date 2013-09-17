78336301afebc60ca9626093134b7e2e253ae5b6
Noah Feehan <nfeehan@gmail.com>
2013-09-17 13:36:21 -0400

    adding text method to prevent linebreaks from cutting words in half

--- parseTest.py
+++ parseTest.py
@@ -7,17 +7,18 @@ printer.upsideDownOn()
 
 def parse(text):
     r = text.split(' ')
-    # r.reverse()
     curLine = ''
     fin = []
     tally = 0
     for w in r:
-    	if len(w)+len(curLine) > (maxCol-1):
-    		fin.append(curLine)
-    		curLine = ''
-    		curLine+=w
-    	else:
-    		curLine+=' '+w
-    print curLine
+        if len(w)+len(curLine) > (maxCol-1):
+            fin.append(curLine)
+            curLine = ''
+            curLine+=w
+        else:
+            curLine+=' '+w
+    # print curLine
     fin.append(curLine)
-    print '\n'.join(fin)
+    rt = '\n'.join(fin)
+    rt.lstrip(' ')
+    print rt
