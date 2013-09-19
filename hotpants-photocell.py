0ecb95453d4165de10792340c242a35ed2c4b76f
Noah Feehan <nfeehan@gmail.com>
2013-09-19 17:34:19 -0400

    reverse sensor range

--- hotpants-photocell.py
+++ hotpants-photocell.py
@@ -77,7 +77,7 @@ def checkSensor():
     delta = r-rPast
     # delta = r-avg
 
-    # print(r, delta, avg, rMin, rMax)
+    print('%s\t%s\t%s\t%s\t%s'%(r, delta, avg, rMin, rMax))
     
     if r > rMax:
         rMax = r
